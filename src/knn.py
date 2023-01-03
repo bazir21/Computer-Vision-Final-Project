e744825e89147d0de400994c6f1a3c4ef1036416
swenginator <72451636+swenginator@users.noreply.github.com>
2023-01-03 18:59:36 +0100

    Cross-validate knn

--- src/knn.py
+++ src/knn.py
@@ -1,14 +1,17 @@
+import time
+
 import numpy as np
-from segmentation import prepare_training_data
+from sklearn.metrics import accuracy_score
+from sklearn.model_selection import KFold
 from sklearn.neighbors import KNeighborsClassifier
-from sklearn.model_selection import train_test_split, KFold
-from sklearn.metrics import classification_report, accuracy_score
-from sklearn.svm import SVC
+
+from segmentation import prepare_training_data
+
 
 # all ripped straight from here lol! 🥴🥴
 # https://pyimagesearch.com/2021/04/17/your-first-image-classifier-using-k-nn-to-classify-images/
 
-def knn():
+def main():
     x, y = prepare_training_data(dimensions=(32, 32))
     x = np.array(x)
     x = x.reshape((x.shape[0], (x.shape[1] * x.shape[2] * x.shape[3])))
@@ -16,27 +19,26 @@ def knn():
     y = np.array(y, dtype=np.uint8)
 
     kf = KFold(n_splits=5)
-    # model = SVC(kernel="linear")
-    model = KNeighborsClassifier(n_neighbors=7, n_jobs=-1)
+    for k in range(1, 20):
+        print(f"Running with k={k}")
+        model = KNeighborsClassifier(n_neighbors=k, n_jobs=-1)
 
-    accuracy = []
+        accuracy = []
 
-    for train, test in kf.split(y):
-        model.fit(x[train], y[train])
-        y_pred = model.predict(x[test])
-        accuracy.append(accuracy_score(y[test], y_pred))
+        start_time = time.time()
+        for train, test in kf.split(y):
+            model.fit(x[train], y[train])
+            y_pred = model.predict(x[test])
+            accuracy.append(accuracy_score(y[test], y_pred))
 
-    mean_value = np.array(accuracy).mean()
-    std = np.array(accuracy).std()
+        mean_value = np.array(accuracy).mean()
+        std = np.array(accuracy).std()
+        taken = time.time() - start_time
 
-    print(f'Mean: {mean_value}')
-    print(f'STD: {std}')
+        print(f'Took {taken}s')
+        print(f'Mean: {mean_value}')
+        print(f'STD: {std}')
 
-    # x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.2)
 
-    # model = SVC(kernel="linear")
-    # model = KNeighborsClassifier(n_neighbors=7)
-    # model.fit(x_train, y_train)
-    # print(classification_report(y_test, model.predict(x_test)))
-
-knn()
+if __name__ == "__main__":
+    main()
