2142d053ab4346d40a48b79601d290f9b80c715c
John Peter Keaney <keaneyjo@ITSERV.SCSS.TCD.IE>
2023-01-05 17:51:06 +0000

    AlexNet confusion matrix

---
+++ src/checkgpu.py
@@ -0,0 +1,11 @@
+import tensorflow as tf
+import torch
+print(torch.cuda.is_available())
+print(torch.cuda.get_device_properties(0).name)
+
+print("Num GPUs Available: ", len(tf.config.list_physical_devices('GPU')))
+print(tf.config.list_physical_devices('GPU'))
+
+gpus = tf.config.experimental.list_physical_devices('GPU')
+for gpu in gpus:
+    tf.config.experimental.set_memory_growth(gpu, True)
